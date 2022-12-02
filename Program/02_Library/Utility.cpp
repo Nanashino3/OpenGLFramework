d45260010af4f13e9e067c332285c0dfd2d89775
ta.kusumoto <ta.kusumoto11827@gmail.com>
2022-12-02 14:32:16 +0900

    CSVファイルの読み込みを実装

--- Program/02_Library/Utility.cpp
+++ Program/02_Library/Utility.cpp
@@ -1,6 +1,8 @@
 #include "Utility.h"
 
 #include <wrl.h>
+#include <fstream>
+#include <sstream>
 
 namespace tkl
 {
@@ -27,6 +29,27 @@ void ToWChara(wchar_t* wstrDest, const char* strSrc, int length)
 {
 	int result = MultiByteToWideChar(CP_ACP, 0, strSrc, -1, wstrDest, length);
 	wstrDest[length - 1] = 0;
+}
+
+std::vector<std::vector<std::string>> LoadCsv(const char* filename)
+{
+	std::vector<std::vector<std::string>> result;
+
+	std::ifstream ifs(filename);
+
+	std::string line;
+	while(getline(ifs, line)){
+		std::istringstream iss(line);
+
+		std::string s;
+		std::vector<std::string> datas;
+		while(getline(iss, s, ',')){
+			datas.emplace_back(s);
+		}
+		result.emplace_back(datas);
+	}
+
+	return result;
 }
 
 } // namespace tkl
--- Program/02_Library/Utility.h
+++ Program/02_Library/Utility.h
@@ -3,10 +3,12 @@
 #include <cstdio>
 #include <cstdint>
 #include <string>
+#include <vector>
 
 namespace tkl
 {
 	wchar_t ToOnceWChara(const char* strSrc);
 	void ToWChara(wchar_t* wstrDest, uint32_t destLegth, const std::string& src);
 	void ToWChara(wchar_t* wstrDest, const char* strSrc, int length);
+	std::vector<std::vector<std::string>> LoadCsv(const char* filename);
 } // namespace tkl
