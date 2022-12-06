0449f2195d9bb1d99b434a3e969ff4d1834efe90
ta.kusumoto <ta.kusumoto11827@gmail.com>
2022-12-06 21:17:24 +0900

    TODO：GLFWでのFPS管理がわからないので一旦Windowsの機能に頼る

--- Program/Main.cpp
+++ Program/Main.cpp
@@ -1,6 +1,13 @@
+#include <chrono>
+#include <Windows.h>
+#pragma comment(lib,"winmm.lib")
+
 #include <iostream>
 #include "00_Game/GameMain.h"
 #include "01_Engine/System.h"
+
+static std::chrono::system_clock::time_point clock_start, clock_end;
+static std::chrono::system_clock::time_point fps_clock_start, fps_clock_end;
 
 int main()
 {
@@ -9,20 +16,46 @@ int main()
 
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
-	float clockStart = glfwGetTime(), clockEnd = 0.0f;
-	const float interval = 1.0f/60.0f;
+//	float clockStart = static_cast<float>(glfwGetTime()), clockEnd = 0.0f;
+//	const float interval = 1.0f/60.0f;
 
 	while(!system->ProcessMessage()){
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// フレーム間の経過時間
-		clockEnd = static_cast<float>(glfwGetTime());
-		float deltaTime = clockEnd - clockStart;
-		if(deltaTime >= interval){ clockStart = clockEnd; }
+#if 1
+		clock_end = std::chrono::system_clock::now();
+		double micro_seconds = static_cast<double>(std::chrono::duration_cast<std::chrono::microseconds>(clock_end - clock_start).count());
+		float deltaTime = static_cast<float>(micro_seconds / 1000.0 / 1000.0);
+		clock_start = clock_end;
+
+		fps_clock_start = std::chrono::system_clock::now();
 
 		GameMain(deltaTime);
 
 		system->SwapBuffers();
+
+		// フレームレートコントロール
+		fps_clock_end = std::chrono::system_clock::now();
+		
+		double fps_mic = static_cast<double>(std::chrono::duration_cast<std::chrono::microseconds>(fps_clock_end - fps_clock_start).count());
+		float fps_mils = static_cast<float>(fps_mic / 1000.0);
+		float fps_lim = 1000.0f / 60.0f;
+		
+		if (fps_lim > fps_mils) {
+			timeBeginPeriod(1);
+			Sleep(DWORD(fps_lim - fps_mils));
+			timeEndPeriod(1);
+		}
+#else
+		// フレーム間の経過時間
+		clockEnd = static_cast<float>(glfwGetTime());
+		float deltaTime = clockEnd - clockStart;
+		if (deltaTime >= interval) { clockStart = clockEnd; }
+
+		GameMain(deltaTime);
+
+		system->SwapBuffers();
+#endif
 	}
 
 	GameEnd();
