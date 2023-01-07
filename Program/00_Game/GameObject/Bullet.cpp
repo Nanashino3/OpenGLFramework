a6a93c3418bef4db8fbb35c7c40498b7b008ae93
ta.kusumoto <ta.kusumoto11827@gmail.com>
2023-01-08 01:46:05 +0900

    弾クラスの清書

--- Program/00_Game/GameObject/Bullet.cpp
+++ Program/00_Game/GameObject/Bullet.cpp
@@ -10,19 +10,25 @@
 #include "../../01_Engine/Intersect.h"
 #include "../../01_Engine/Graphics/Geometry/Mesh.h"
 
+// ファイルパス
+static constexpr const char* TEXTURE_FILE = "Resource/texture/panel_concrete.bmp";
+
+// 定数
 static constexpr int BULLET_SIZE = 2;
+static constexpr int DIVISION_W = 24;
+static constexpr int DIVISION_H = 16;
+static constexpr int DAMAGE = 5;
+static constexpr float OBJECT_HEIGHT = 9.5f;
 static constexpr float DESTROY_POS = 70.0f;
 static constexpr float MOVE_SPEED = 100.0f;
-static constexpr int DAMAGE = 5;
+static constexpr float RECT_SIZE = 8.5f;
 
 Bullet::Bullet(std::shared_ptr<Parameter> param)
 : mRadian(0)
 , mMesh(nullptr)
+, mParam(nullptr)
 {
 	mParam = std::dynamic_pointer_cast<GameParameter>(param);
-
-	mMesh = tkl::Mesh::CreateSphere(BULLET_SIZE, 24, 16);
-	mMesh->SetTexture(tkl::ResourceManager::GetInstance()->CreateTextureFromFile("Resource/texture/panel_concrete.bmp"));
 }
 
 Bullet::~Bullet()
@@ -35,14 +41,15 @@ Bullet::~Bullet()
 // 戻り値：なし
 // 詳　細：弾クラスの初期化処理
 //****************************************************************************
-//void Bullet::Initialize()
 void Bullet::Initialize(const tkl::Vector3& srcPos, const tkl::Vector3& target)
 {
 	tkl::Vector3 diff = target - srcPos;
 	mRadian = atan2f(diff.mZ, diff.mX);
 	mLauncherPos = srcPos;
 
-	mMesh->SetPosition(tkl::Vector3(srcPos.mX, 9.5f, srcPos.mZ));
+	mMesh = tkl::Mesh::CreateSphere(BULLET_SIZE, DIVISION_W, DIVISION_H);
+	mMesh->SetTexture(tkl::ResourceManager::GetInstance()->CreateTextureFromFile(TEXTURE_FILE));
+	mMesh->SetPosition(tkl::Vector3(srcPos.mX, OBJECT_HEIGHT, srcPos.mZ));
 }
 
 //****************************************************************************
@@ -63,7 +70,8 @@ void Bullet::Collision()
 		tkl::Vector3 unitPos = unit->GetPosition();
 
 		// 進軍ユニットと衝突していたら弾削除
-		if(tkl::IsIntersectAABB(pos, BULLET_SIZE, unitPos, 8.5f)){
+		// TODO：進軍ユニットのサイズを取得できるようにする
+		if(tkl::IsIntersectAABB(pos, BULLET_SIZE, unitPos, RECT_SIZE)){
 			mIsAlive = false;
 			// TODO：与えるダメージは可変にしたい
 			unit->ReceiveDamage(DAMAGE);
